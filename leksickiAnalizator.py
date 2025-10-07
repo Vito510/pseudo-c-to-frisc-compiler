995b5623ad0484fbd59cfedf0a3273f79bb7da60
Vito Čuić <vito.cuic@fer.hr>
2025-10-07 21:20:58 +0200

    Implement lexical analysis for C code with regex matching and enhance regex pattern conversion

--- leksickiAnalizator.py
+++ leksickiAnalizator.py
@@ -1,3 +1,4 @@
+import re
 import parserLeksickogAnalizatora
 
 data = parserLeksickogAnalizatora.parse("./data/c-leksik-pravila.txt")
@@ -6,4 +7,29 @@ stanja = data.stanja
 jedinke = data.jedinke
 prijelazi = data.prijelazi
 
-print(stanja,jedinke,prijelazi)
+
+stanje = stanja[0] # pocetno stanje
+c_file = open("./data/c-program.c", "r", encoding="utf-8").read()
+
+buffer = []
+carape = [0]*60
+
+
+c_file = "0x1234"
+for s in c_file:
+    # print(s)
+    buffer.append(s)
+    for i, ulaz in enumerate(prijelazi[stanje]):
+        
+        poen = 1 if re.fullmatch(ulaz, "".join(buffer)) is not None else 0
+        carape[i] += poen
+
+
+    print("".join(map(str,carape)))
+
+
+
+for i in range(len(carape)):
+    if carape[i] > 0:
+        k = list(prijelazi[stanje].keys())[i]
+        print(prijelazi[stanje][k],k)
--- parserLeksickogAnalizatora.py
+++ parserLeksickogAnalizatora.py
@@ -5,26 +5,28 @@ class ParserData:
         self.jedinke = jedinke
         self.prijelazi = prijelazi
 
-reg_definicije = {}
+# def ispis_reg_def() -> None:
+#     for k in reg_definicije:
+#         print(f'{k:40}\t{"".join(reg_definicije[k])}')
 
-def ispis_reg_def() -> None:
-    for k in reg_definicije:
-        print(f'{k:40}\t{"".join(reg_definicije[k])}')
-
-def convert_2_pattern(pattern: str) -> str:
+def convert_2_pattern(pattern: str, reg_definicije: dict = {}) -> str:
     """converts the given regex pattern to python applicable format""" 
     for key in reg_definicije:
         key2 = "{" + key + "}"
+    
         if key2+"|" in pattern: # ne moramo enkapsulirati
             pattern = pattern.replace(key2,f'{reg_definicije[key]}')
         elif key2 in pattern:   # moramo
             pattern = pattern.replace(key2,f'({reg_definicije[key]})')
-    pattern = pattern.replace("(","[").replace(")","]") # python regex uses [] insted of ()
-    if not (pattern.startswith('[') and pattern.endswith(']') or pattern.endswith('*')) : pattern = f'[{pattern}]'      # mora biti enkapsulirano sa [], ako vec nije
+    
+    escape = ["^", "[", "+", "-", "$", "]","?","."]
+    for item in escape:
+        pattern = pattern.replace(item,"\\"+item)
+    
     return pattern
 
-def parse(filepath: str) -> ParserData: 
-
+def parse(filepath: str) -> ParserData:
+    reg_definicije = {}
     pravila_txt = open(filepath, "r", encoding="utf-8").readlines()
 
     for i, line in enumerate(pravila_txt):
@@ -37,6 +39,14 @@ def parse(filepath: str) -> ParserData:
         key, value = line.split(" ")
         key = key[1:-1]
 
+        for k in reg_definicije:
+            key2 = "{" + k + "}"
+        
+            if key2+"|" in value: # ne moramo enkapsulirati
+                value = value.replace(key2,f'{reg_definicije[k]}')
+            elif key2 in value:   # moramo
+                value = value.replace(key2,f'({reg_definicije[k]})')
+        
         reg_definicije[key] = value
 
     pravila_txt = pravila_txt[i:]
@@ -61,7 +71,7 @@ def parse(filepath: str) -> ParserData:
         stanje,r = prijelaz.split(">",maxsplit=1)
         ulaz, akcija = map(str.strip,r.rsplit("{",maxsplit=1))
         akcija = list(filter(lambda x: x != "-", akcija.split("\n")))
-        ulaz = convert_2_pattern(ulaz)
+        ulaz = convert_2_pattern(ulaz, reg_definicije)
         # print(stanje,ulaz,akcija)
 
         if stanje not in prijelazi:
