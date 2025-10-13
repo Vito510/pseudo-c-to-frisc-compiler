33705fbb4b779fe2b6897ada9f3f60077b79a075
Vito Čuić <vito.cuic@fer.hr>
2025-10-13 12:11:53 +0200

    Add initial test cases and configuration for unittest framework

---
+++ data/c-test.c
@@ -0,0 +1,15 @@
+struct S {
+    char t;
+    int x;
+};
+
+/*
+ne radi nista
+*/
+void fun(int xYz) {
+    return;
+}
+
+// glavni program "testira osnovne kljucne rijeci i operatore za lekser"
+int main(void) {
+    int A[512];
---
+++ tests/test_leksik.py
@@ -0,0 +1,53 @@
+import unittest
+from parserLeksickogAnalizatora import parse, _convert_2_pattern,_reg_definicije
+from regex import match
+
+class Test(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Parse test
+        """
+        result = parse("./data/c-leksik-pravila.txt")
+        self.assertIsNotNone(result)
+        
+    def test_regex_creator(self):
+        """
+        Tests patern convertsion
+        """
+        
+        test = ["{znamenka}",
+                "(e|E)($|+|-){znamenka}{znamenka}*",
+                "(_|{znak})(_|{znak}|{znamenka})*",
+                "{znamenka}{znamenka}*"]
+        
+        correct = [
+                "(0|1|2|3|4|5|6|7|8|9)",
+                "(e|E)($|+|-)(0|1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*",
+                "(_|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z)(_|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|0|1|2|3|4|5|6|7|8|9)*",
+                "(0|1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*"]
+        
+
+        for t, c in zip(test, correct):
+            result = _convert_2_pattern(t,_reg_definicije)
+            self.assertEqual(result,c)
+            
+    def test_regex_match(self):
+        """
+        Tests pattern matching
+        """
+        test = [("a","(a|b|c)"),
+                ("512];","(0|1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*"),
+                ("e+1005abc","(e|E)(\\$|\\+|-)(1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*"),
+                ("v+1002","(e|E)(\\$|\\+|-)(1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*"),
+                ("aabbabbc","(a|b)*bc")]
+        
+        correct = ["a","512","e+1005",None,"aabbabbc"]
+        
+
+        for t, c in zip(test, correct):
+            result = match(t[1],t[0])
+            self.assertEqual(result,c)
+        
+
+if __name__ == "__main_":
+    unittest.main()
